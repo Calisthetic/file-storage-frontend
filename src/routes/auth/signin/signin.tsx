8313813feb433823a6541cb7e0b6ec84891ad9a9
Calisthetic <vovakalash04@mail.ru>
2023-08-01 05:46:11 +0500

    animations

--- src/routes/auth/signin/signin.tsx
+++ src/routes/auth/signin/signin.tsx
@@ -13,62 +13,62 @@ export default function SignIn() {
 
   return (
     <div className=" min-h-fulldvh flex justify-center items-center">
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <motion.img initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.0}}
-        className="mx-auto h-12 w-auto" src={mainLogo} alt="Logo" />
-        <motion.h2 initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.02, damping: 24, stiffness: 300}}
-        className="mt-10 text-center text-xl sm:text-2xl font-bold leading-9 tracking-tight text-textLight dark:text-textDark">Sign in to your account</motion.h2>
-      </div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <motion.img initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.0}}
+          className="mx-auto h-12 w-auto" src={mainLogo} alt="Logo" />
+          <motion.h2 initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.02, damping: 24, stiffness: 300}}
+          className="mt-10 text-center text-xl sm:text-2xl font-bold leading-9 tracking-tight text-textLight dark:text-textDark">Sign in to your account</motion.h2>
+        </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.04, damping: 24, stiffness: 300}}
-            className="block text-sm font-medium leading-6 text-textLight dark:text-textDark">Email address</motion.label>
-            <div className="mt-2">
-              <motion.input initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.06, damping: 24, stiffness: 300}}
-               id="email" name="email" type="email" autoComplete="email" required 
-              className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
-              ring-1 ring-inset ring-borderLight dark:ring-borderDark placeholder:text-gray-400 
-              focus:outline-offset-0 text-sm sm:text-base sm:leading-6" />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="javascript:void(0);">
+            <div>
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.04, damping: 24, stiffness: 300}}
+              className="block text-sm font-medium leading-6 text-textLight dark:text-textDark">Email address</motion.label>
+              <div className="mt-2">
+                <motion.input initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.06, damping: 24, stiffness: 300}}
+                id="email" name="email" type="email" autoComplete="email" required 
+                className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
+                ring-1 ring-inset ring-borderLight dark:ring-borderDark placeholder:text-gray-400 
+                focus:outline-offset-0 text-sm sm:text-base sm:leading-6" />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.08, damping: 24, stiffness: 300}} 
-              className="block text-sm font-medium leading-6 text-textLight dark:text-textDark">Password</motion.label>
-                <motion.a initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1, damping: 24, stiffness: 300}}
-                 href="#" className="text-sm font-semibold text-buttonLight dark:text-buttonDark 
-                hover:text-buttonHoverLight dark:hover:text-buttonHoverDark">Forgot password?</motion.a>
-              
+            <div>
+              <div className="flex items-center justify-between">
+                <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.08, damping: 24, stiffness: 300}} 
+                className="block text-sm font-medium leading-6 text-textLight dark:text-textDark">Password</motion.label>
+                  <motion.a initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1, damping: 24, stiffness: 300}}
+                  href="#" className="text-sm font-semibold text-buttonLight dark:text-buttonDark 
+                  hover:text-buttonHoverLight dark:hover:text-buttonHoverDark">Forgot password?</motion.a>
+                
+              </div>
+              <div className="mt-2">
+                <motion.input initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.12, damping: 24, stiffness: 300}}
+                id="password" name="password" type="password" autoComplete="current-password" required 
+                className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
+                ring-1 ring-inset ring-borderLight dark:ring-borderDark placeholder:text-gray-400 
+                focus:outline-offset-0 text-sm sm:text-base sm:leading-6" />
+              </div>
             </div>
-            <div className="mt-2">
-              <motion.input initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.12, damping: 24, stiffness: 300}}
-               id="password" name="password" type="password" autoComplete="current-password" required 
-              className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
-              ring-1 ring-inset ring-borderLight dark:ring-borderDark placeholder:text-gray-400 
-              focus:outline-offset-0 text-sm sm:text-base sm:leading-6" />
-            </div>
-          </div>
 
-          <div>
-            <motion.button initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.14, damping: 24, stiffness: 300}}
-             type="submit" className="flex w-full justify-center rounded-md bg-buttonLight dark:bg-buttonDark 
+            <div>
+              <motion.button initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.14, damping: 24, stiffness: 300}}
+              type="submit" className="flex w-full justify-center rounded-md bg-buttonLight dark:bg-buttonDark 
               hover:bg-buttonHoverLight dark:hover:bg-buttonHoverDark px-3 py-1.5 text-sm font-semibold leading-6 
-              text-textLight shadow-sm dark:text-textDark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</motion.button>
-          </div>
-        </form>
+              text-textLight shadow-sm dark:text-textDark">Sign in</motion.button>
+            </div>
+          </form>
 
-        <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.16, damping: 24, stiffness: 300}}
-          className="mt-10 text-center text-sm text-textLight dark:text-textDark ">
-          <span className='opacity-70'>Not a member?</span>
-          <Link to="/auth/signup" className="font-semibold leading-6 text-buttonLight dark:text-buttonDark 
-            hover:text-buttonHoverLight dark:hover:text-buttonHoverDark"> Start a 14 day free trial</Link>
-        </motion.div>
+          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.16, damping: 24, stiffness: 300}}
+            className="mt-10 text-center text-sm text-textLight dark:text-textDark ">
+            <span className='opacity-70'>Not a member?</span>
+            <Link to="/auth/signup" className="font-semibold leading-6 text-buttonLight dark:text-buttonDark 
+              hover:text-buttonHoverLight dark:hover:text-buttonHoverDark"> Start a 14 day free trial</Link>
+          </motion.div>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
--- src/routes/auth/signup/signup.tsx
+++ src/routes/auth/signup/signup.tsx
@@ -4,97 +4,103 @@ import { Link } from 'react-router-dom';
 export default function SignUp() {
   return (
     <div className=" min-h-fulldvh sm:flex justify-center items-center">
-    <div className='py-6 flex flex-col justify-center min-h-fulldvh sm:h-auto bg-backgroundLight dark:bg-backgroundDark 
-    sm:rounded-xl w-fullcvw sm:w-auto px-10'>
-      <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0, damping: 24, stiffness: 300}} 
-      className="text-xl sm:text-2xl w-full pb-6 text-center font-semibold leading-6 text-textLight dark:text-textDark">Sign Up</motion.p>
+      <form className='py-6 flex flex-col justify-center min-h-fulldvh sm:h-auto bg-backgroundLight dark:bg-backgroundDark 
+      sm:rounded-xl w-fullcvw sm:w-auto px-10' action="javascript:void(0);">
+        <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.00, damping: 24, stiffness: 300}} 
+        className="text-xl sm:text-2xl w-full pb-6 text-center font-semibold leading-6 text-textLight dark:text-textDark">Sign Up</motion.p>
 
-      <div className="border-y border-borderLight dark:border-borderDark py-6">
-        <div className=" grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-4">
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.02, damping: 24, stiffness: 300}}
-           className="sm:col-span-2 col-span-full">
-            <label title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">First name 
-              <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
-            </label>
-            <div className="pt-2">
-              <input type="text" name="first-name" id="first-name" autoComplete="given-name" 
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
-              ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
-              sm:text-sm sm:leading-6 transition-inputs"/>
+        <div className="border-y border-borderLight dark:border-borderDark py-6">
+          <div className=" grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-4">
+            <div className="sm:col-span-2 col-span-full">
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.02, damping: 24, stiffness: 300}}
+              title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">First name 
+                <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
+              </motion.label>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.04, damping: 24, stiffness: 300}}
+              className="pt-2">
+                <input type="text" name="first-name" id="first-name" autoComplete="given-name" required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
+                ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
+                sm:text-sm sm:leading-6 transition-inputs"/>
+              </motion.div>
             </div>
-          </motion.div>
 
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.04, damping: 24, stiffness: 300}}
-          className="sm:col-span-2 col-span-full">
-            <label title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Last name
-              <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
-            </label>
-            <div className="pt-2">
-              <input type="text" name="last-name" id="last-name" autoComplete="family-name" 
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
-              ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
-              sm:text-sm sm:leading-6 transition-inputs"/>
+            <div className="sm:col-span-2 col-span-full">
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.06, damping: 24, stiffness: 300}}
+              title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Last name
+                <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
+              </motion.label>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.08, damping: 24, stiffness: 300}}
+              className="pt-2">
+                <input type="text" name="last-name" id="last-name" autoComplete="family-name" required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
+                ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
+                sm:text-sm sm:leading-6 transition-inputs"/>
+              </motion.div>
             </div>
-          </motion.div>
 
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.06, damping: 24, stiffness: 300}}
-          className="col-span-full">
-            <label title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Email address
-              <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
-            </label>
-            <div className="pt-2">
-              <input id="email" name="email" type="email" autoComplete="email" 
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
-              ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
-              sm:text-sm sm:leading-6 transition-inputs"/>
+            <div className="col-span-full">
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1, damping: 24, stiffness: 300}}
+              title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Email address
+                <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
+              </motion.label>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.12, damping: 24, stiffness: 300}}
+              className="pt-2">
+                <input id="email" name="email" type="email" autoComplete="email" required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
+                ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
+                sm:text-sm sm:leading-6 transition-inputs"/>
+              </motion.div>
+              <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.14, damping: 24, stiffness: 300}}
+              className="pt-1 text-sm leading-6 text-textLight dark:text-textDark">Use a permanent address where you can receive mail.</motion.p>
             </div>
-            <p className="pt-1 text-sm leading-6 text-textLight dark:text-textDark">Use a permanent address where you can receive mail.</p>
-          </motion.div>
 
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.08, damping: 24, stiffness: 300}}
-           className="col-span-full pt-2">
-            <label title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Password
-              <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
-            </label>
-            <div className="pt-2">
-              <input id="password" name="password" type="password" autoComplete="password" 
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
-              ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
-              sm:text-sm sm:leading-6 transition-inputs"/>
+            <div className="col-span-full pt-2">
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.16, damping: 24, stiffness: 300}} 
+              title='required' className="block text-base font-medium leading-4 text-textLight dark:text-textDark">Password
+                <span title='required' className=" text-iconLight dark:text-iconDark">*</span>
+              </motion.label>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.18, damping: 24, stiffness: 300}} 
+              className="pt-2">
+                <input id="password" name="password" type="password" autoComplete="password" required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
+                ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
+                sm:text-sm sm:leading-6 transition-inputs"/>
+              </motion.div>
+              <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.2, damping: 24, stiffness: 300}} 
+              className="pt-1 text-sm leading-6 text-textLight dark:text-textDark">Repeat password</motion.p>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.22, damping: 24, stiffness: 300}} 
+              className="pt-2">
+                <input id="password" name="password" type="password" autoComplete="password" required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
+                ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
+                sm:text-sm sm:leading-6 transition-inputs"/>
+              </motion.div>
             </div>
-          </motion.div>
 
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1, damping: 24, stiffness: 300}}
-           className="col-span-full">
-            <p className="text-sm leading-4 text-textLight dark:text-textDark">Repeat password</p>
-            <div className="pt-2">
-              <input id="password" name="password" type="password" autoComplete="password" 
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 
-              ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-none 
-              sm:text-sm sm:leading-6 transition-inputs"/>
+            <div className="col-span-full pt-2">
+              <motion.label initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.24, damping: 24, stiffness: 300}}
+              className="block text-base font-medium leading-4 text-textLight dark:text-textDark">About</motion.label>
+              <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.26, damping: 24, stiffness: 300}}
+              className="pt-2">
+                <textarea id="about" name="about" rows={3} className="block w-full rounded-md border-0 p-1.5 
+                text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                focus:ring-none sm:text-sm sm:leading-6 transition-inputs"></textarea>
+              </motion.div>
+              <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.28, damping: 24, stiffness: 300}}
+              className="pt-3 text-sm leading-6 text-textLight dark:text-textDark">Write a few sentences about yourself.</motion.p>
             </div>
-          </motion.div>
-
-          <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.12, damping: 24, stiffness: 300}}
-           className="col-span-full pt-2">
-            <label className="block text-base font-medium leading-4 text-textLight dark:text-textDark">About</label>
-            <div className="pt-2">
-              <textarea id="about" name="about" rows={3} className="block w-full rounded-md border-0 p-1.5 
-              text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-              focus:ring-none sm:text-sm sm:leading-6 transition-inputs"></textarea>
-            </div>
-            <p className="pt-3 text-sm leading-6 text-textLight dark:text-textDark">Write a few sentences about yourself.</p>
-          </motion.div>
+          </div>
         </div>
-      </div>
 
-      <div className="pt-4 flex items-center text-base justify-between gap-x-6">
-        <Link to="../signin" className="text-sm font-semibold leading-6 text-textLight dark:text-textDark
-        hover:text-buttonHoverLight dark:hover:text-buttonHoverDark transition-colors">Already have an account?</Link>
-        <button type="submit" className="rounded-md dark:bg-buttonDark bg-buttonLight px-3 py-2 text-sm font-semibold 
-        text-textLight dark:text-textDark shadow-sm hover:bg-buttonHoverLight dark:hover:to-buttonHoverDark transition-colors">Send</button>
-      </div>
-    </div>
+        <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{delay: 0.3, damping: 24, stiffness: 300}}
+        className="pt-4 flex items-center text-base justify-between gap-x-6">
+          <Link to="../signin" className="text-sm font-semibold leading-6 text-textLight dark:text-textDark
+          hover:text-buttonHoverLight dark:hover:text-buttonHoverDark transition-colors">Already have an account?</Link>
+          <button type="submit" className="rounded-md dark:bg-buttonDark bg-buttonLight px-3 py-2 text-sm font-semibold 
+          text-textLight dark:text-textDark shadow-sm hover:bg-buttonHoverLight dark:hover:to-buttonHoverDark transition-colors">Send</button>
+        </motion.div>
+      </form>
     </div>
   )
 }
