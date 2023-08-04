3e34a5cfe564b7026420d88df35dfeb9899d2e10
Calisthetic <vovakalash04@mail.ru>
2023-08-04 13:18:13 +0500

    sort ui

--- src/routes/disk/folder/disk-folder.tsx
+++ src/routes/disk/folder/disk-folder.tsx
@@ -18,7 +18,7 @@ export default function DiskFolder() {
   const [isSortDrop, setIsSortDrop] = useState(true)
   const [currentRenderType, setCurrentRenderType] = useState("list")
   const [currentSortType, setCurrentSortType] = useState("name")
-  const [isCurrentSortByDescending, setIsCurrentSortByDescending] = useState(false)
+  const [currentSortBy, setCurrentSortBy] = useState("ascending")
 
   function VisualizeAddDrop() {
     setIsAddDrop(!isAddDrop)
@@ -159,10 +159,12 @@ export default function DiskFolder() {
             font-medium rounded-full text-base sm:text-lg w-10 h-10 sm:w-11 sm:h-11 text-center justify-center inline-flex items-center transition-dropDown
             focus:bg-backgroundThirdLight focus:dark:bg-backgroundThirdDark first-letter:uppercase" 
             type="button">
-              <svg viewBox="0 0 24 24" className="w-7 h-7" xmlns="http://www.w3.org/2000/svg">
+              <motion.svg initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} 
+              transition={{damping: 24, duration: 0.25, stiffness: 300}}
+              viewBox="0 0 24 24" className="w-7 h-7" xmlns="http://www.w3.org/2000/svg">
                 <path d="m6 20 4-4H7V4H5v12H2zm5-12h9v2h-9zm0 4h7v2h-7zm0-8h11v2H11zm0 12h5v2h-5z" 
                 className="fill-iconLight dark:fill-iconDark"></path>
-              </svg>
+              </motion.svg>
             </button>
             <div id="dropdown" className="z-10 opacity-0 divide-y divide-gray-100 rounded w-44 mt-0.5
             absolute transition-dropDown -translate-x-3/4 
@@ -255,6 +257,49 @@ export default function DiskFolder() {
                 </li>
                 <li>
                   <div className="border my-1 border-borderLight dark:border-borderDark"></div>
+                </li>
+                {/* is order by descending */}
+                <li>
+                  <div className="cursor-pointer transition-colors px-2 py-1.5 
+                  hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark flex flex-row justify-start items-center
+                  bg-backgroundThirdLight dark:bg-backgroundThirdDark" data-popup="true"
+                  data-name="ascending" onClick={(e:any) => {setCurrentSortBy(e.target.dataset.name)}}>
+                    <AnimatePresence>
+                      {currentSortBy === "ascending" && (
+                        <motion.svg initial={{x: -50}} animate={{x: 0}} exit={{x: -50}}
+                        transition={{damping: 24, stiffness: 300, duration: 0.25}}
+                        viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4 pointer-events-none absolute">
+                          <path d="M480 128c0 8.188-3.125 16.38-9.375 22.62l-256 256C208.4 412.9 200.2 416 192 
+                          416s-16.38-3.125-22.62-9.375l-128-128C35.13 272.4 32 264.2 32 256c0-18.28 14.95-32 32-32 
+                          8.188 0 16.38 3.125 22.62 9.375L192 338.8l233.4-233.4c6.2-6.27 14.4-9.4 22.6-9.4 17.1 0 
+                          32 13.7 32 32z" className=" fill-iconLight dark:fill-iconDark"></path>
+                        </motion.svg>
+                      )}
+                    </AnimatePresence>
+                    <span className="pointer-events-none ml-6">Ascending</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="cursor-pointer transition-colors px-2 py-1.5 
+                  hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark flex flex-row justify-start items-center
+                  bg-backgroundThirdLight dark:bg-backgroundThirdDark" data-popup="true"
+                  data-name="descending" onClick={(e:any) => {setCurrentSortBy(e.target.dataset.name)}}>
+                    <AnimatePresence>
+                      {currentSortBy === "descending" && (
+                        <motion.svg initial={{x: -50}} animate={{x: 0}} exit={{x: -50}}
+                        transition={{damping: 24, stiffness: 300, duration: 0.25}}
+                        viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4 pointer-events-none absolute">
+                          <path d="M480 128c0 8.188-3.125 16.38-9.375 22.62l-256 256C208.4 412.9 200.2 416 192 
+                          416s-16.38-3.125-22.62-9.375l-128-128C35.13 272.4 32 264.2 32 256c0-18.28 14.95-32 32-32 
+                          8.188 0 16.38 3.125 22.62 9.375L192 338.8l233.4-233.4c6.2-6.27 14.4-9.4 22.6-9.4 17.1 0 
+                          32 13.7 32 32z" className=" fill-iconLight dark:fill-iconDark"></path>
+                        </motion.svg>
+                      )}
+                    </AnimatePresence>
+                    <span className="pointer-events-none ml-6">Descending</span>
+                  </div>
                 </li>
               </ul>
             </div>
