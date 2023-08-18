868e5afb554028a4df41607b635516355cef4ade
Calisthetic <vovakalash04@mail.ru>
2023-08-18 19:08:32 +0500

    click test

--- src/routes/disk/folder/render-data.tsx
+++ src/routes/disk/folder/render-data.tsx
@@ -613,7 +613,7 @@ export default function RenderData({currentSortType, currentSortBy, currentRende
                                 <button className="rounded-full h-6 w-6 transition-shadow
                                 hover:shadow-defaultLight hover:dark:shadow-defaultDark"
                                 style={{backgroundColor: "#" + primary_color.color}}
-                                title={primary_color.name}>
+                                title={primary_color.name} onClick={() => {alert("Clicked")}}>
                                   {item.color?.toLowerCase() === primary_color.color.toLowerCase() && (
                                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" 
                                     enableBackground="new 0 0 24 24" className="w-6 h-6">
@@ -940,7 +940,26 @@ export default function RenderData({currentSortType, currentSortBy, currentRende
           </div>
         </div>
       ) : currentRenderType === "table" ? (
-        <div className="sm:rounded-t-lg">
+        <div>
+          <div className="font-bold bg-backgroundLight dark:bg-backgroundDark text-textLight dark:text-textDark uppercase mb-10
+          w-full whitespace-nowrap overflow-y-visible text-base text-left flex flex-row items-center sm:rounded-t-lg">
+            <div className="h-8 w-8 py-1 px-1 md:w-12 md:px-3">
+              <svg viewBox="0 0 256 256" className="w-6 h-6" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h256v256H0z"></path>
+                <path d="M224 177.3V78.7a8.1 8.1 0 0 0-4.1-7l-88-49.5a7.8 7.8 0 
+                0 0-7.8 0l-88 49.5a8.1 8.1 0 0 0-4.1 7v98.6a8.1 8.1 0 0 0 4.1 7l88 
+                49.5a7.8 7.8 0 0 0 7.8 0l88-49.5a8.1 8.1 0 0 0 4.1-7Z" 
+                fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" 
+                className=" stroke-textLight dark:stroke-textDark"></path>
+                <path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" 
+                d="M177 152.5v-52L80 47" className="stroke-textLight dark:stroke-textDark"></path>
+                <path fill="none" className="stroke-textLight dark:stroke-textDark" 
+                strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" 
+                d="m222.9 74.6-94 53.4-95.8-53.4M128.9 128l-.9 106.8"></path>
+              </svg>
+            </div>
+            <div>name</div>
+          </div>
           <table className="w-full whitespace-nowrap overflow-y-visible text-base text-left text-textLight dark:text-textDark">
             <thead className="uppercase font-normal bg-backgroundLight dark:bg-backgroundDark text-textLight dark:text-textDark">
               <tr>
