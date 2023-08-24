03776caa6cbb03bb5a2e3a5fee237f690aabfae4
Calisthetic <vovakalash04@mail.ru>
2023-08-24 21:45:09 +0500

    new copy interface

--- src/routes/disk/folder/folder-access-modal.tsx
+++ src/routes/disk/folder/folder-access-modal.tsx
@@ -30,6 +30,16 @@ export default function FolderAccessModal({children, folderId, folderName, folde
     }
   }
 
+  function CopyButtonClick() {
+    if (currentPage === "default") {
+      CopyText(folderToken)
+    } else {
+      if (generatedToken !== null) {
+        GenerateToken()
+      }
+    }
+  }
+
 
   // Generate new url
   const [generatedToken, setGeneratedToken] = useState<string | undefined | null>()
@@ -80,11 +90,13 @@ export default function FolderAccessModal({children, folderId, folderName, folde
 
   function GenerateToken() {
     setGeneratedToken(null)
+    let newToken = "dksdskdlsk"
 
 
     
     setTimeout(() => {
-      setGeneratedToken("rkkfjfk")
+      setGeneratedToken(newToken)
+      CopyText(newToken)
     }, 1000);
   }
 
@@ -318,6 +330,14 @@ export default function FolderAccessModal({children, folderId, folderName, folde
         </>
       ) : ( // generate
         <>
+          {/* Back button */}
+          <button className="mt-2 rounded-md p-1.5 transition-colors gap-x-2 w-full
+          bg-backgroundLight dark:bg-backgroundDark flex flex-row items-center justify-center
+          hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark
+          border border-borderLight dark:border-borderDark font-medium"
+          onClick={() => {setCurrentPage("default")}}>
+            <span>Back</span>
+          </button>
           {/* password */}
           <div className="bg-backgroundSecondLight dark:bg-backgroundSecondDark rounded-md
           flex flex-row mt-2 items-center relative gap-x-2">
@@ -491,93 +511,60 @@ export default function FolderAccessModal({children, folderId, folderName, folde
             </label>
             <p>Require authorization</p>
           </div>
-          {/* Generate new url button */}
-          <button className="mt-2 rounded-md p-1.5 transition-colors gap-x-2 w-full
-          bg-backgroundThirdLight dark:bg-backgroundThirdDark flex flex-row items-center
-          hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark
-          border border-borderLight dark:border-borderDark font-medium"
-          onClick={GenerateToken}>
-            {generatedToken === null ? (
-              <>
-                <svg aria-hidden="true" role="status" className="w-4 h-4 animate-spin" 
-                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 
-                  100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 
-                  100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 
-                  91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 
-                  9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" 
-                  className="fill-iconLight dark:fill-iconDark"/>
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 
-                  28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 
-                  7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 
-                  0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 
-                  9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 
-                  10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 
-                  21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 
-                  38.2158 91.5421 39.6781 93.9676 39.0409Z" 
-                  className="fill-iconLight dark:fill-iconDark"/>
-                </svg>
-                <span>Loading...</span>
-              </>
-            ) : (
-              <span>Generate token</span>
-            )}
-          </button>
-          {/* Copy place */}
-          <div className="bg-backgroundSecondLight dark:bg-backgroundSecondDark rounded-md p-1.5
-          flex flex-row mt-2 items-center relative gap-x-2 justify-between w-full">
-            <p className="truncate max-w-[240px] sm:max-w-[40dvw] xl:max-w-[540px]">ddddddddddddd sssssssssssss dddddddddddddd ssssssssssss dddddddddddddd sssssss</p>
-            <button className="bg-backgroundSecondLight dark:bg-backgroundSecondDark
-            hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark transition-colors
-            border border-borderLight dark:border-borderDark rounded-md p-1"
-            onClick={() => {CopyText(generatedToken)}}>
-              <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" className="h-5 w-5">
-                <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 
-                0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 
-                0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"
-                className="fill-iconLight dark:fill-iconDark"></path>
-                <path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 
-                1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 
-                .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"
-                className="fill-iconLight dark:fill-iconDark"></path>
-              </svg>
-            </button>
-          </div>
         </>
       )}
       {/* Buttons */}
       <div className="flex flex-row justify-between font-medium mt-4">
-        {currentPage === "default" ? (
-          <button className="flex flex-row items-center py-1 px-3 rounded-full
-          border border-borderLight dark:border-borderDark transition-colors
-          hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark"
-          onClick={() => {CopyText(folderToken)}}>
-            <svg viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg" 
-            className="w-5 h-5">
-              <path d="M598.6 41.41C570.1 13.8 534.8 0 498.6 0s-72.36 13.8-99.96 41.41l-43.36 43.36c15.11 8.012 
-              29.47 17.58 41.91 30.02 3.146 3.146 5.898 6.518 8.742 9.838l37.96-37.96C458.5 72.05 477.1 64 498.6 
-              64c20.67 0 40.1 8.047 54.71 22.66 14.61 14.61 22.66 34.04 22.66 54.71s-8.049 40.1-22.66 54.71l-133.3 
-              133.3C405.5 343.1 386 352 365.4 352s-40.1-8.048-54.71-22.66C296 314.7 287.1 295.3 287.1 274.6s8.047-40.1 
-              22.66-54.71l4.44-3.49c-2.1-3.9-4.3-7.9-7.5-11.1-8.6-8.6-19.9-13.3-32.1-13.3-11.93 0-23.1 4.664-31.61 
-              12.97-30.71 53.96-23.63 123.6 22.39 169.6C293 402.2 329.2 416 365.4 416c36.18 0 72.36-13.8 99.96-41.41L598.6 
-              241.3c28.45-28.45 42.24-66.01 41.37-103.3-.87-35.9-14.57-69.84-41.37-96.59zM234 387.4l-37.9 37.9C181.5 
-              439.1 162 448 141.4 448c-20.67 0-40.1-8.047-54.71-22.66-14.61-14.61-22.66-34.04-22.66-54.71s8.049-40.1 
-              22.66-54.71l133.3-133.3C234.5 168 253.1 160 274.6 160s40.1 8.048 54.71 22.66c14.62 14.61 22.66 34.04 
-              22.66 54.71s-8.047 40.1-22.66 54.71l-3.51 3.52c2.094 3.939 4.219 7.895 7.465 11.15C341.9 315.3 353.3 
-              320 365.4 320c11.93 0 23.1-4.664 31.61-12.97 30.71-53.96 23.63-123.6-22.39-169.6C346.1 109.8 310.8 96 
-              274.6 96c-36.2 0-72.3 13.8-99.9 41.4L41.41 270.7C13.81 298.3 0 334.48 0 370.66c0 36.18 13.8 72.36 41.41 
-              99.97C69.01 498.2 105.2 512 141.4 512c36.18 0 
-              72.36-13.8 99.96-41.41l43.36-43.36c-15.11-8.012-29.47-17.58-41.91-30.02-3.21-3.11-5.91-6.51-8.81-9.81z" 
-              className=" fill-textLight dark:fill-textDark"></path>
-            </svg>
-            <p className="pointer-events-none ml-2">Copy link</p>
-          </button>
-        ) : (
-          <button className="flex flex-row items-center py-1 px-4 rounded-full
-          border border-borderLight dark:border-borderDark transition-colors
-          hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark"
-          onClick={() => {setCurrentPage("default")}}>Back</button>
-        )}
+        <button className="flex flex-row items-center py-1 px-3 rounded-full
+        border border-borderLight dark:border-borderDark transition-colors
+        hover:bg-backgroundHoverLight hover:dark:bg-backgroundHoverDark"
+        onClick={CopyButtonClick}>
+          {generatedToken === null ? (
+            <>
+              <svg aria-hidden="true" role="status" className="w-4 h-4 animate-spin" 
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 
+                78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 
+                100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 
+                91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 
+                9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                className="fill-textLight dark:fill-textDark"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 
+                28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 
+                7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 
+                0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 
+                9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 
+                10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 
+                21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 
+                38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                className="fill-textLight dark:fill-textDark"/>
+              </svg>
+              <span className="pointer-events-none ml-2">Loading...</span>
+            </>
+          ) : (
+            <>
+              <svg viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg" 
+              className="w-5 h-5">
+                <path d="M598.6 41.41C570.1 13.8 534.8 0 498.6 0s-72.36 13.8-99.96 41.41l-43.36 43.36c15.11 8.012 
+                29.47 17.58 41.91 30.02 3.146 3.146 5.898 6.518 8.742 9.838l37.96-37.96C458.5 72.05 477.1 64 498.6 
+                64c20.67 0 40.1 8.047 54.71 22.66 14.61 14.61 22.66 34.04 22.66 54.71s-8.049 40.1-22.66 54.71l-133.3 
+                133.3C405.5 343.1 386 352 365.4 352s-40.1-8.048-54.71-22.66C296 314.7 287.1 295.3 287.1 274.6s8.047-40.1 
+                22.66-54.71l4.44-3.49c-2.1-3.9-4.3-7.9-7.5-11.1-8.6-8.6-19.9-13.3-32.1-13.3-11.93 0-23.1 4.664-31.61 
+                12.97-30.71 53.96-23.63 123.6 22.39 169.6C293 402.2 329.2 416 365.4 416c36.18 0 72.36-13.8 99.96-41.41L598.6 
+                241.3c28.45-28.45 42.24-66.01 41.37-103.3-.87-35.9-14.57-69.84-41.37-96.59zM234 387.4l-37.9 37.9C181.5 
+                439.1 162 448 141.4 448c-20.67 0-40.1-8.047-54.71-22.66-14.61-14.61-22.66-34.04-22.66-54.71s8.049-40.1 
+                22.66-54.71l133.3-133.3C234.5 168 253.1 160 274.6 160s40.1 8.048 54.71 22.66c14.62 14.61 22.66 34.04 
+                22.66 54.71s-8.047 40.1-22.66 54.71l-3.51 3.52c2.094 3.939 4.219 7.895 7.465 11.15C341.9 315.3 353.3 
+                320 365.4 320c11.93 0 23.1-4.664 31.61-12.97 30.71-53.96 23.63-123.6-22.39-169.6C346.1 109.8 310.8 96 
+                274.6 96c-36.2 0-72.3 13.8-99.9 41.4L41.41 270.7C13.81 298.3 0 334.48 0 370.66c0 36.18 13.8 72.36 41.41 
+                99.97C69.01 498.2 105.2 512 141.4 512c36.18 0 
+                72.36-13.8 99.96-41.41l43.36-43.36c-15.11-8.012-29.47-17.58-41.91-30.02-3.21-3.11-5.91-6.51-8.81-9.81z" 
+                className=" fill-textLight dark:fill-textDark"></path>
+              </svg>
+              <span className="pointer-events-none ml-2">Copy link</span>
+            </>
+          )}
+        </button>
         {/* Close modal button */}
         {children}
       </div>
