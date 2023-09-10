7a30416cb41cc495933520fbe8f601eb2eb54f45
Calisthetic <vovakalash04@mail.ru>
2023-09-10 14:16:20 +0500

    add RotateArray()

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,10 +15,7 @@ export function CutNumber(num: number, stage: number = 0):string {
   ) : CutNumber(Math.floor(num / 1000), stage+1)
 }
 
-export function IsNumeric(n:any) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
+// Date
 export function GetCurrentYear():number {
   var currentTime = new Date()
   return currentTime.getFullYear()
@@ -28,6 +25,18 @@ export function GetCurrentDate():string {
   var dd = String(today.getDate()).padStart(2, '0');
   var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
-  
   return yyyy + '-' + mm + '-' + dd;
+}
+
+export function IsNumeric(n:any) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+export function RotateArray(arr:string[]) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = temp
+  }
+  return arr
 }
