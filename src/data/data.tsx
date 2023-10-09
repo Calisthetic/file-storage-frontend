a362562e8f2a6e97e631656441078c52e1b77647
Calisthetic <vovakalash04@mail.ru>
2023-10-09 22:40:52 +0500

    start writing docs

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,3 +1,4 @@
 export const selfUrl:string = "https://file-storage-frontend.vercel.app/"
 export const apiUrl:string = "https://localhost:7089/v1.0/"
 //export const apiUrl:string = "https://rpkztvjg-7144.euw.devtunnels.ms/api/main/v1.0/"
+export const apiDocs:string = "https://rpkztvjg-7144.euw.devtunnels.ms/swagger"
