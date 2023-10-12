16b480a89ad948ecc2dca82fc74176165ab6c1ae
Calisthetic <vovakalash04@mail.ru>
2023-10-12 22:05:13 +0500

    dropdown with css only

--- src/components/user-profile-dropdown.tsx
+++ src/components/user-profile-dropdown.tsx
@@ -1,92 +1,56 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion"
+import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
-
-interface UserProfileDropdownProps {
-  
-}
  
-const UserProfileDropdown: FunctionComponent<UserProfileDropdownProps> = () => {
-  const [isUserDropMenuOpen, setIsUserDropMenuOpen] = useState(false)
-
-  // Close modal event
-  const root = document.getElementById("root")
-  useEffect(() => {
-    if (root) {
-      root.addEventListener("click", (e:any) => {
-        SetDropdown(e)
-      })
-
-      return () => {
-        root.removeEventListener("click", (e:any) => {
-          SetDropdown(e)
-        })
-      }
-    }
-  }, [root])
-  function SetDropdown(e:any) {
-    //e.preventDefault()
-    if (e.target.dataset.drop !== "userMenu") {
-      setIsUserDropMenuOpen(false)
-    }
-  }
-
-
+const UserProfileDropdown: FunctionComponent = () => {
   // User image url
   let temp:string | null = localStorage.getItem("userImage")
   const userImage:string | undefined = temp === null ? undefined : temp
 
 
-
   let token = localStorage.getItem("token")
   return token ? (
     <div className="flex items-center">
-      <div className="flex items-center ml-3">
-        <button onClick={() => {setIsUserDropMenuOpen(!isUserDropMenuOpen)}} 
-        data-drop="userMenu" className="flex text-sm bg-gray-800 rounded-full">
+      <div className="flex ml-3">
+        <button className="flex text-sm bg-backgroundThirdLight dark:bg-backgroundThirdDark 
+        rounded-full peer focus:pointer-events-none">
           <img className="w-8 h-8 rounded-full pointer-events-none" alt="user" draggable="false"
           src={userImage} />
         </button>
-        <AnimatePresence>
-          {isUserDropMenuOpen ? (
-            <motion.div initial={{opacity: 0, y: 44, scaleY: 0.2, x: "calc(-100% + 32px)"}} 
-            animate={{opacity: 1, y: "calc(50% + 18px)", scaleY: 1}}
-            exit={{opacity: 0, y: 44, scaleY: 0.2}}
-            transition={{stiffness: 200, damping: 24, duration: 0.16}}
-            className="absolute z-50 my-4 text-base list-none bg-backgroundThirdLight 
-            dark:bg-backgroundThirdDark dark:divide-borderDark text-textLight dark:text-textDark
-            shadow-lightLight dark:shadow-lightDark rounded mb-2">
-              <div className="px-4 py-3">
-                <p className="">
-                  Neil Sims
-                </p>
-                <p className="font-medium">
-                  neil.sims@flowbite.com
-                </p>
-              </div>
-              <div className="py-1">
-                <Link to="/welcome" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Home
-                </Link>
-                <Link to="/disk/folder/main" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Disk
-                </Link>
-                <Link to="/user/profile" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Profile
-                </Link>
-                <Link to="/user/statistic" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Statistic
-                </Link>
-                <Link to="/docs" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Documentation
-                </Link>
-                <Link to="/auth" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
-                  dark:hover:bg-backgroundHoverDark" role="menuitem">Sign out
-                </Link>
-              </div>
-            </motion.div>
-          ) : null}
-        </AnimatePresence>
+        <div className="grid absolute grid-rows-[0fr] peer-focus:grid-rows-[1fr] focus-within:grid-rows-[1fr]
+        -translate-x-[calc(100%-32px)] duration-300 w-max mt-10 transition-[grid-template-rows,margin]">
+          <div className="z-50 text-base list-none bg-backgroundThirdLight 
+          dark:bg-backgroundThirdDark dark:divide-borderDark text-textLight dark:text-textDark
+          shadow-lightLight dark:shadow-lightDark rounded overflow-hidden">
+            <div className="px-4 py-3">
+              <p className="">
+                Neil Sims
+              </p>
+              <p className="font-medium">
+                neil.sims@flowbite.com
+              </p>
+            </div>
+            <div className="py-1">
+              <Link to="/welcome" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Home
+              </Link>
+              <Link to="/disk/folder/main" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Disk
+              </Link>
+              <Link to="/user/profile" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Profile
+              </Link>
+              <Link to="/user/statistic" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Statistic
+              </Link>
+              <Link to="/docs" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Documentation
+              </Link>
+              <Link to="/auth/signin" className="block px-4 py-2 hover:bg-backgroundHoverLight w-full text-left
+                dark:hover:bg-backgroundHoverDark" role="menuitem">Sign out
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
    ) : (
